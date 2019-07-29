29f464eac3e3f4ae0d39a5c007fc28bb06e24c07
Mario Andres Portela Gaviria <mario.portela@wolox.co>
2019-07-29 15:09:51 -0500

    refactoring of albums resolvers

--- app/graphql/albums/resolvers.js
+++ app/graphql/albums/resolvers.js
@@ -4,13 +4,9 @@ const { findAlbumById, findPhotosByAlbumId, findAlbums } = require('../../servic
 exports.getAlbum = (parent, params) => findAlbumById(params.id);
 exports.getPhotos = parent => findPhotosByAlbumId(parent.id);
 exports.albumsList = async (parent, params) => {
-  let albumsProcessed = [];
   const { limit, offset, orderBy, filter } = params;
   const albums = await findAlbums();
-  if (filter) {
-    albumsProcessed = filterArrayByField(albums, 'title', filter);
-  } else {
-    albumsProcessed = albums;
-  }
+  const albumsProcessed = filter ? filterArrayByField(albums, 'title', filter) : albums;
+
   return orderArrayByField(albumsProcessed.slice(offset, offset + limit), orderBy);
 };
